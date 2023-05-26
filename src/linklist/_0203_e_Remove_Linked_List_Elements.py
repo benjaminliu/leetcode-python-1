43614ec2224c78dc81f11cadbb6a0ecc2e26c482
ashley <benjamin.liu@live.cn>
2023-05-25 20:25:47 -0400

    _0203_e_Remove_Linked_List_Elements

---
+++ src/linklist/_0203_e_Remove_Linked_List_Elements.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+from common.ListNode import ListNode
+
+
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        dummy = ListNode()
+        dummy.next = head
+
+        cur = dummy
+
+        while cur.next:
+            if cur.next.val == val:
+                cur.next = cur.next.next
+            else:
+                cur = cur.next
+
+        return dummy.next
